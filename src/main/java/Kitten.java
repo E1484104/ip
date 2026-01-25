2f1e6c3306b62962039b55fd44dc6346e239919c
e1484104 <e1484104@u.nus.edu>
2026-01-25 14:54:04 +0800

    Add "add" and "list" function

--- src/main/java/Kitten.java
+++ src/main/java/Kitten.java
@@ -7,12 +7,25 @@ public class Kitten {
         System.out.println("     What can I do for you? I'm always here to be with you~");
         System.out.println("    ____________________________________________________________\n");
 
+        String[] tasks = new String[100];
+        int index = 0;
+
         String line;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
         while(!line.equals("bye")) {
-            System.out.println("     " + line);
-            System.out.println("    ____________________________________________________________\n");
+            if(line.equals("list")){
+                for(int i = 1; i <= index; i++) {
+                    System.out.println("     " + i + ". " + tasks[i-1]);
+                }
+                System.out.println("    ____________________________________________________________\n");
+            }else{
+                System.out.println("     Task added: " + line);
+                System.out.println("    ____________________________________________________________\n");
+                tasks[index] = line;
+                index++;
+            }
+
             line = in.nextLine();
         }
 
