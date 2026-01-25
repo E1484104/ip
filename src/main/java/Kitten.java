db5f60d2c9efac5580cb2da2ddc7a8aafa1efdf9
e1484104 <e1484104@u.nus.edu>
2026-01-25 14:35:22 +0800

    Customize Kitten's language pack

--- src/main/java/Kitten.java
+++ src/main/java/Kitten.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Kitten {
     public static void main(String[] args) {
         System.out.println("    ____________________________________________________________");
-        System.out.println("     Hello! I'm Kitten");
-        System.out.println("     What can I do for you?");
+        System.out.println("     Hey! I'm Kitten~ Good to see you again!");
+        System.out.println("     What can I do for you? I'm always here to be with you~");
         System.out.println("    ____________________________________________________________\n");
 
         String line;
@@ -16,7 +16,7 @@ public class Kitten {
             line = in.nextLine();
         }
 
-        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("     Bye~ Have a nice day and all the best!");
         System.out.println("    ____________________________________________________________");
     }
 }
