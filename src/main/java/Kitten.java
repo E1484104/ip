e166f6220dfbf87c9c82d12c2fa504d81fd3d4fe
e1484104 <e1484104@u.nus.edu>
2026-01-25 14:23:19 +0800

    Make Kitten echo commands

--- src/main/java/Kitten.java
+++ src/main/java/Kitten.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class Kitten {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Kitten");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hello! I'm Kitten");
+        System.out.println("     What can I do for you?");
+        System.out.println("    ____________________________________________________________\n");
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while(!line.equals("bye")) {
+            System.out.println("     " + line);
+            System.out.println("    ____________________________________________________________\n");
+            line = in.nextLine();
+        }
+
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
     }
 }
